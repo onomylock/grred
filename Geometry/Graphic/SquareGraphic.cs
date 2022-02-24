e3af36a2097dcb18c1c2ed074758129f978ee51c
Anna <anyadun51@gmail.com>
2022-02-24 10:49:16 +0000

    перенесли отрисовку треугольника

--- Geometry/Graphic/SquareGraphic.cs
+++ Geometry/Graphic/SquareGraphic.cs
@@ -1,4 +1,4 @@
-﻿namespace GrRed.Geometry.Graphic
+namespace GrRed.Geometry.Graphic
 {
     class SquareGraphic : IGraphic
     {
@@ -13,9 +13,31 @@
             //
         }
 
-        void AddLines(IEnumerable<Vector> lines) { };
-        void FillPolygon(IEnumerable<Vector> lines) { };
-        void AddPolyArc(IEnumerable<Vector> lines) { };// каждые 3 точки -- дуга окружности
-        void FillPolyArc(IEnumerable<Vector> lines) { };// каждые 3 точки -- дуга окружности
+        void AddLines(IEnumerable<Vector> lines) 
+        { 
+            PathGeometry pathGeom = new PathGeometry();
+            Path path = new Path();
+            PolyLineSegment polyLine = new PolyLineSegment();
+            PathFigure pathFig = new PathFigure();
+
+            pathFig.StartPoint = new Point(lines[0].X, lines[0].Y);
+            Point[] polyLinePointArray =
+                new Point[] { new Point(lines[1].X, lines[1].Y), new Point(lines[2].X, lines[2].Y), new Point(lines[3].X, lines[3].Y) };
+            polyLine.Points =
+                 new PointCollection(polyLinePointArray);
+            pathFig.Segments.Add(polyLine);
+            pathFig.IsClosed = true;
+            pathGeom.Figures.Add(pathFig);
+            path.Data = pathGeom;
+            path.Stroke = Brushes.Black;
+            PaintingGrid.Children.Add(path);
+
+        };
+        
+        void FillPolygon(IEnumerable<Vector> lines) 
+        {
+            path.Fill = Brushes.Red;
+        };
+
     }
 }
