351a1758830a0895b5569cf20824dd81d407645f
it.mitsuk <it.mitsuk@gmail.com>
2022-03-05 20:45:11 +0700

    Добавлен интерфейс ICommand ActionCommand под интерфейс ICommand

---
+++ gui2/gui/Interface/ICommand.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace gui.Interface
+{
+    interface ICommand
+    {
+        event EventHandler CanExecuteChanged;
+        void Execute(object parameter);
+        bool CanExecute(object parameter);
+    }
+}
