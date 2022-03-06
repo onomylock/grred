57cba7f2fbfc9e956e410c9d3a35b17a82f812c2
it.mitsuk <it.mitsuk@gmail.com>
2022-03-06 19:52:07 +0700

    Переход на  MVVM

---
+++ gui/ButtonCommands.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace gui
+{
+    class ButtonCommands
+    {
+        #region RightButtons
+        private void ReflectHorizontally()
+        {
+            bool axe = true;
+        }
+
+        private void ReflectVertically()
+        {
+            bool axe = false;
+        }
+        #endregion
+
+        #region LeftButtons
+
+        #endregion
+
+        #region ICommand
+        //public ICommand ReflectHorizontallyCommand
+        //{
+        //    get
+        //    {
+        //        return new ActionCommand((sender, true) =>
+        //        {
+        //            ReflectHorizontally();
+        //        });
+        //    }
+        //}
+        #endregion
+    }
+}
