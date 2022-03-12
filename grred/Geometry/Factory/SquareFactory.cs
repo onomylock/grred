bcb55fb2cb69823c6b7b77570abb26e97def16cf
onomylock <onomylock@outlook.com>
2022-03-12 16:05:21 +0700

    Добавил перегрузку метода GetFigure

--- grred/Geometry/Factory/SquareFactory.cs
+++ grred/Geometry/Factory/SquareFactory.cs
@@ -1,9 +1,14 @@
 ﻿using GrRed.Geometry.Domain;
 
+using System.Collections.Generic;
 namespace GrRed.Geometry.Factory
 {
     public class SquareFactory : FigureFactory
     {
+        public override IFigure GetFigure(IEnumerable<Vector> Points)
+        {
+            return new Square(Points);
+        }
         public override IFigure GetFigure(double Angle, Vector Center, Vector Scale)
         {
             return new Square(Angle, Center, Scale);
